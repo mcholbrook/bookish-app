d6301f5f527368994252ca708858209b550ecf74
mcholbrook <mirandacholbrook@gmail.com>
2020-12-14 18:35:39 -0500

    GCorrectly return search results

--- controllers/books.js
+++ controllers/books.js
@@ -3,8 +3,41 @@ const axios = require('axios')
 
 module.exports = {
   index,
+  new: newBook,
+  search, 
 }
 
 function index(req, res){
   res.render('books/index', {title: "Browse All Books", user: req.user})
+}
+
+function newBook(req, res){
+  res.render('books/new', {title: 'Search for a Book', user: req.user, results: null})
+}
+
+function search(req, res){
+  axios
+  .get(`https://www.googleapis.com/books/v1/volumes?q=${req. body.query}&limit=5`)
+  .then((response) => {
+    console.log(response.data.items)
+    res.render('books/new', {
+      title: 'Search for a Book',
+      user: req.user,
+      results: response.data.items
+    })
+  })
+    // .get(`http://openlibrary.org/search.json?q=${req. body.query}&limit=5`)
+    // .then((response) => {
+    //   console.log(response.data.docs)
+    //   res.render('books/new', {
+    //     title: 'Search for a Book',
+    //     user: req.user,
+    //     results: response
+    //   })
+    // })
+  // console.log('This is a test')
+  // res.render('books/new', {
+  //   title: 'This was a successful Search!',
+  //   user: req.user
+  // })
 }
--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const booksCtrl = require('../controllers/books')
 
 router.get('/index', isLoggedIn, booksCtrl.index)
+router.get('/new', isLoggedIn, booksCtrl.new)
+router.post('/search', isLoggedIn, booksCtrl.search)
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
