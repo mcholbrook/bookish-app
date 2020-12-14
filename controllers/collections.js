6282a313a992ebd586d942053565276164936284
mcholbrook <mirandacholbrook@gmail.com>
2020-12-14 12:52:50 -0500

    Add collections route

--- controllers/collections.js
+++ controllers/collections.js
@@ -1,6 +1,15 @@
 const Collection = require('../models/collection')
 const Book = require('../models/book')
+const User = require('../models/user')
 
 module.exports = {
-  
+  index
+}
+
+function index(req, res){
+  User.findById(req.user._id)
+  .then((user) => {
+    console.log(`This is ${req.user.name}'s Collections`)
+    res.render('collections/index', {title: 'My Collections', user})
+  })
 }
--- routes/collections.js
+++ routes/collections.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const collectionsCtrl = require('../controllers/collections')
 
+router.get('/index', collectionsCtrl.index)
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
   res.redirect("/auth/google");
