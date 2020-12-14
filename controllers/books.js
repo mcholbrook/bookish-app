5978ec9ef5acce82c25f3ec0587189e13e83b7f9
mcholbrook <mirandacholbrook@gmail.com>
2020-12-14 12:39:10 -0500

    Add book index route

--- controllers/books.js
+++ controllers/books.js
@@ -2,5 +2,9 @@ const Book = require('../models/book')
 const axios = require('axios')
 
 module.exports = {
-  
+  index,
+}
+
+function index(req, res){
+  res.render('books/index', {title: "Browse All Books", user: req.user})
 }
--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const booksCtrl = require('../controllers/books')
 
+router.get('/index', isLoggedIn, booksCtrl.index)
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
   res.redirect("/auth/google");
