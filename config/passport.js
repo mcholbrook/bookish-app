890135ca3ba9f318496262dcb60b83d7a1aca43d
mcholbrook <mirandacholbrook@gmail.com>
2020-12-16 12:07:30 -0500

    Update user login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user");
+const Collection
 
 passport.use(
   new GoogleStrategy(
