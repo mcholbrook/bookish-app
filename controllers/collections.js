5c9d16886fd968280a6e11c9ce740433f972990c
mcholbrook <mirandacholbrook@gmail.com>
2020-12-16 23:51:44 -0500

    CCollection title and image display on index

--- controllers/collections.js
+++ controllers/collections.js
@@ -11,6 +11,7 @@ module.exports = {
 function index(req, res){
   // res.render('collections/index', {title: 'My Collections', user: req.user} )
   User.findById(req.user._id)
+  .populate('collections')
   .then((user) => {
     console.log(`This is ${req.user.name}'s Collections`)
     res.render('collections/index', {title: 'My Collections', user})
